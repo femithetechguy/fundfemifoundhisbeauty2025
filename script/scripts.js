7ee6c75a35332578ae3ee142f583578bcc7c783b
Tech Guy <femithetechguy@gmail.com>
2025-09-16 19:33:12 -0400

    updated zelle and cashapp info

--- script/scripts.js
+++ script/scripts.js
@@ -7,14 +7,13 @@ fetch('json/funds.json')
     data.funds.forEach(fund => {
       const fundDiv = document.createElement('div');
       fundDiv.className = 'fund-item';
-      // Determine image type for alt text
-      let imgType = '';
-      if (fund.qr_image.endsWith('.png')) imgType = 'PNG';
-      else if (fund.qr_image.endsWith('.jpeg') || fund.qr_image.endsWith('.jpg')) imgType = 'JPEG';
+      // Use a wrapper to enforce square aspect ratio for QR
       fundDiv.innerHTML = `
         <h2>${fund.name}</h2>
         <p>ID: <span class="fund-id">${fund.id}</span></p>
-        <img src="${fund.qr_image}" alt="${fund.name} QR (${imgType})" class="fund-qr" />
+        <div class="qr-wrapper">
+          <img src="${fund.qr_image}" alt="${fund.name} QR" class="fund-qr" />
+        </div>
       `;
       fundsContainer.appendChild(fundDiv);
     });
